e91db9c2836cdd7695fbc5075741818a4a8e36a0
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-28 11:49:27 -0700

    commit tree 0c2b0be19d8569a48550bfe96e5b53524f109345

---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: {scope: %i[release_year artist_name], message: "no repeated info!"}
+    validates :released, inclusion: {in: [true, false]}
+    validates :artist_name, presence:true
+    
+    with_options if: :released do |song|
+        song.validates :release_year, presence: true
+        song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+    end
+end
